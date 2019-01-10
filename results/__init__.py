982757dcdd7b840de31b6dbbf8a839c93cd6fc0d
S.I. Mimilakis <jsmimilak@gmail.com>
2019-01-10 16:31:27 +0100

    Added link here

--- results/__init__.py
+++ results/__init__.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 __author__ = 'S.I. Mimilakis'
 __copyright__ = 'MacSeNet'
-
-
-
-
-
-
-
+"""
+    Please copy in this folder the results from the optimization
+    found in the *.pytorch files. Corresponding link:
+    https://seafile.idmt.fraunhofer.de/d/8724ff410d714d6fb437/
+"""
 
 # EOF
